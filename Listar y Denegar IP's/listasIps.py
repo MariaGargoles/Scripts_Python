b44aaf4d317e27541d199445360edb6d4010a8cc
María Gárgoles <mariagargoles@live.com>
2024-04-12 11:24:36 +0200

    ADD estraer

--- Listar y Denegar IP's/listasIps.py
+++ Listar y Denegar IP's/listasIps.py
@@ -1,50 +1,39 @@
 #!/usr/bin/env python3
 import subprocess
+import csv
 from datetime import datetime, timedelta
 
-# Obtener la fecha actual
 fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Cabecera del fichero IPs_maliciosas.txt
-with open('/home/carpeta/IPs_maliciosas.txt', 'w') as f:
-    f.write("---------------------------------------------------------------------------------------------------------------------------\n")
-    f.write("\n")
-    f.write("-------------- IPs maliciosas desde las que se hacen ataques por fuerza bruta contra nuestro sistema. --------------\n")
-    f.write("\n")
-    f.write("---------------------------------------------------------------------------------------------------------------------------\n")
+with open('/home/carpeta/IPs_maliciosas.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Fecha', 'Hora', 'IP', 'Port', 'Nombre']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
+    writer.writeheader()
 
-# Pasar por teclado el número de días atrás que queremos filtrar el journalctl
-ndias = input(f"Hola {subprocess.getoutput('echo $USER')}, ¿de cuántos días quieres filtrar el journalctl? ")
-ndias = int(ndias)
+    ndias = input(f"Hola {subprocess.getoutput('echo $USER')}, ¿de cuántos días quieres filtrar el journalctl? ")
+    ndias = int(ndias)
 
-# Consultar el journalctl y filtrarlo por +invalid user+. El resultado se guarda en el fichero IPs_maliciosas.txt que está en /home/carpeta
-journal_output = subprocess.getoutput(f"sudo journalctl -S -{ndias}d | grep 'invalid user'")
-with open('/home/carpeta/IPs_maliciosas.txt', 'a') as f:
+    journal_output = subprocess.getoutput(f"sudo journalctl -S -{ndias}d | grep 'invalid user'")
     for line in journal_output.splitlines():
         parts = line.split()
         if parts[11] != "port":
-            f.write(f"Fecha: {parts[0]} {parts[1]}\tHora: {parts[2]}\tIP: {parts[11]}\tPort: {parts[13]}\tNombre: {parts[10]}\n")
+            writer.writerow({'Fecha': parts[0], 'Hora': parts[1], 'IP': parts[11], 'Port': parts[13], 'Nombre': parts[10]})
         else:
-            f.write(f"\tFecha: {parts[0]} {parts[1]}\tHora: {parts[2]}\tIP: {parts[10]}\tPort: {parts[12]}\tNombre: desconocido\n")
+            writer.writerow({'Fecha': parts[0], 'Hora': parts[1], 'IP': parts[10], 'Port': parts[12], 'Nombre': 'desconocido'})
 
-# Verificar si el comando se ha ejecutado correctamente
 if subprocess.run(["echo", "$?"], capture_output=True).stdout.decode().strip() == "0":
-    print("El fichero IPs_maliciosas ha sido creado correctamente")
-    # Cambiar los permisos del fichero
-    subprocess.run(["sudo", "chmod", "664", "/home/carpeta/IPs_maliciosas.txt"])
-    # Cambiar el grupo y el propietario del fichero
-    subprocess.run(["sudo", "chown", "seginf_director:carpeta", "/home/carpeta/IPs_maliciosas.txt"])
+    print("El archivo IPs_maliciosas ha sido creado correctamente")
+    subprocess.run(["sudo", "chmod", "664", "/home/carpeta/IPs_maliciosas.csv"])
+    subprocess.run(["sudo", "chown", "seginf_director:carpeta", "/home/carpeta/IPs_maliciosas.csv"])
 
-    # Pie del fichero
-    with open('/home/carpeta/IPs_maliciosas.txt', 'a') as f:
-        f.write("\n")
-        f.write("---------------------------------------------------------------------------------------------------------------------------\n")
-        f.write("\n")
-        f.write(f"Fichero actualizado: {fecha} ----------------------------------------------------------------------------------\n")
-        f.write("---------------------------------------------------------------------------------------------------------------------------\n")
+    with open('/home/carpeta/IPs_maliciosas.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([])
+        writer.writerow([f"Fichero actualizado: {fecha}"])
 else:
-    print("El fichero IPs_maliciosas no ha sido creado correctamente")
+    print("El archivo IPs_maliciosas no ha sido creado correctamente")
 
 respuesta = input("¿Quieres mostrar por pantalla el resultado? 's' para aceptar: ")
 if respuesta.lower() == "s":
-    subprocess.run(["less", "/home/carpeta/IPs_maliciosas.txt"])
+    subprocess.run(["cat", "/home/carpeta/IPs_maliciosas.csv"])
